dc8388fe5d8882bcc0bc3bc0883a186b97a8eec5
wangliyao <755210570@qq.com>
2017-08-21 11:14:24 +0800

    更新readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-使用
-gem 'nokogiri'
-gem "http"
-gem 'pry'
-传入网页参数
-执行 ruby crawier.rb xxx
+##使用
+###gem 'nokogiri'
+###gem "http"
+###gem 'pry'
+###传入网页参数
+###执行 ruby crawier.rb xxx
--- crawler.rb
+++ crawler.rb
@@ -4,6 +4,7 @@ require 'pry'
 
 
 if ARGV[0] != nil
+  binding.pry
   name = ARGV[0]
   body = HTTP.get(name).body
   ans = []
@@ -12,10 +13,8 @@ if ARGV[0] != nil
     if html ==nil
       break;
     end
-    binding.pry
     doc = Nokogiri::HTML(html)
     doc.search('img').each do |i|
-      binding.pry
       ans << i.attr('src') if i.attr('src') != nil
     end
   end
@@ -23,7 +22,6 @@ if ARGV[0] != nil
 # temp_ans = ans.map do  |url|
 #    url.gsub('http://desk.fd.zol-img.com.cn','http://desk.fd.zol-img.com.cn/') if url != nil
 # end
-  binding.pry
 #透过wget 下载档案到当前文件夹
   ans.each do |full_url|
     #system "mkdir #{Dir.getwd}/photo "
